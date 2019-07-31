6c4b058479d87474db26292fb193c5192d859de3
Agnel Vishal <agnelvishal@gmail.com>
2019-07-31 20:51:55 +0530

    Update inputSpace SeperatedValues.c

--- inputSpace SeperatedValues.c
+++ inputSpace SeperatedValues.c
@@ -2,8 +2,10 @@
 int main() {
   int i, b, t, c[10];
   i=0;
-while (i<4 && scanf("%d", &c[i++]));
- 
+
+  //below code from https://stackoverflow.com/questions/9599794/putting-numbers-separated-by-a-space-into-an-array
+  while (i<4 && scanf("%d", &c[i++]));
+
  for (i=0;i<4;i++)
   printf("%d ",c[i]);
   
